e04dd25ca9c094aa3ec0878bc3ea8bc38259fd7c
Neydson-santiago <neydson.anderson@gmail.com>
2025-06-16 21:34:39 +0000

    desafio aventureiro 70% completo

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -3,7 +3,7 @@
 // Desafio Super Trunfo - Países Tema 1 - Cadastro das Cartas
 int main() {
     // Definição de variáveis: estado, código da carta, nome da cidade população, área, PIB, número de pontos turísticos.
-    int turisticosa1,turisticosa2,populacaoa1,populacaoa2;
+    int turisticosa1,turisticosa2,populacaoa1,populacaoa2,opcao;
     float areaa1,areaa2,piba1,piba2,densidadea1,densidadea2,pibpercapa1,pibpercapa2,supera1,supera2;
     char estadoa1[20],estadoa2[20],coda1[20],coda2[20],nomea1[20],nomea2[20];
 
@@ -59,19 +59,116 @@ int main() {
     pibpercapa1 = piba1/populacaoa1;
     pibpercapa2 = piba2/populacaoa2;
 
-    // Resultado da comparação dos Dados das Cartas:
-    
-    printf("Cartao da Cidade A1 - Area: %.2f \n",areaa1);
-    printf("Cartao da Cidade A2 - Area: %.2f \n",areaa2);
+    // Menu para escolha do atributo a ser comparado:
 
-    // Comparação dos Dados das Cartas:
-    // Comparando os valores de area das cidades para decidir o vencedor
-    // Comparação da Area:
-        if(areaa1>areaa2){
-            printf("O Cartão A1 venceu!!! \n");
-        }else {
-            printf("O Cartão A2 venceu!!! \n");
-        }
+    printf("Digite um numero para escolher qual atributo comparar: \n");
+    printf("1 para População \n");
+    printf("2 para Area \n");
+    printf("3 para Pib \n");
+    printf("4 para Pontos Turisticos \n");
+    printf("5 para Densidade Populacional \n");
+    printf("6 para Pib Per Capita \n");
+    printf("7 para Sair \n");
+    scanf("%d", &opcao);
+
+    // Resultado da comparação dos Dados das Cartas dentro do switch:
     
+    switch(opcao) {
+        case 1:
+        if(populacaoa1>populacaoa2){
+            printf("O População da cidade 1: %d\n",populacaoa1);
+            printf("O População da cidade 2: %d\n",populacaoa2);
+            printf("A cidade 1 venceu!!! \n");
+        }else if(populacaoa1<populacaoa2){
+            printf("O População da cidade 1: %d\n",populacaoa1);
+            printf("O População da cidade 2: %d\n",populacaoa2);
+            printf("A cidade 2 venceu!!! \n");
+        } else {
+            printf("O População da cidade 1: %d\n",populacaoa1);
+            printf("O População da cidade 2: %d\n",populacaoa2);
+            printf("Empate!!! \n");
+        } 
+        break;
+        case 2:
+        if(areaa1>areaa2){
+            printf("O Área da cidade 1: %.2f\n",areaa1);
+            printf("O Área da cidade 2: %.2f\n",areaa2);
+            printf("A cidade 1 venceu!!! \n");
+        }else if(areaa1<areaa1){
+            printf("O Área da cidade 1: %.2f\n",areaa1);
+            printf("O Área da cidade 2: %.2f\n",areaa2);
+            printf("A cidade 2 venceu!!! \n");
+        } else {
+            printf("O Área da cidade 1: %.2f\n",areaa1);
+            printf("O Área da cidade 2: %.2f\n",areaa2);
+            printf("Empate!!! \n");
+        }
+        break;
+        case 3:
+        if(areaa1>areaa2){
+            printf("O Área da cidade 1: %.2f\n",areaa1);
+            printf("O Área da cidade 2: %.2f\n",areaa2);
+            printf("A cidade 1 venceu!!! \n");
+        }else if(areaa1<areaa1){
+            printf("O Área da cidade 1: %.2f\n",areaa1);
+            printf("O Área da cidade 2: %.2f\n",areaa2);
+            printf("A cidade 2 venceu!!! \n");
+        } else {
+            printf("O Área da cidade 1: %.2f\n",areaa1);
+            printf("O Área da cidade 2: %.2f\n",areaa2);
+            printf("Empate!!! \n");
+        }
+        break;
+        case 4:
+        if(areaa1>areaa2){
+            printf("O Área da cidade 1: %.2f\n",areaa1);
+            printf("O Área da cidade 2: %.2f\n",areaa2);
+            printf("A cidade 1 venceu!!! \n");
+        }else if(areaa1<areaa1){
+            printf("O Área da cidade 1: %.2f\n",areaa1);
+            printf("O Área da cidade 2: %.2f\n",areaa2);
+            printf("A cidade 2 venceu!!! \n");
+        } else {
+            printf("O Área da cidade 1: %.2f\n",areaa1);
+            printf("O Área da cidade 2: %.2f\n",areaa2);
+            printf("Empate!!! \n");
+        }
+        break;
+        case 5:
+        if(areaa1>areaa2){
+            printf("O Área da cidade 1: %.2f\n",areaa1);
+            printf("O Área da cidade 2: %.2f\n",areaa2);
+            printf("A cidade 1 venceu!!! \n");
+        }else if(areaa1<areaa1){
+            printf("O Área da cidade 1: %.2f\n",areaa1);
+            printf("O Área da cidade 2: %.2f\n",areaa2);
+            printf("A cidade 2 venceu!!! \n");
+        } else {
+            printf("O Área da cidade 1: %.2f\n",areaa1);
+            printf("O Área da cidade 2: %.2f\n",areaa2);
+            printf("Empate!!! \n");
+        }
+        break;
+        case 6:
+        if(areaa1>areaa2){
+            printf("O Área da cidade 1: %.2f\n",areaa1);
+            printf("O Área da cidade 2: %.2f\n",areaa2);
+            printf("A cidade 1 venceu!!! \n");
+        }else if(areaa1<areaa1){
+            printf("O Área da cidade 1: %.2f\n",areaa1);
+            printf("O Área da cidade 2: %.2f\n",areaa2);
+            printf("A cidade 2 venceu!!! \n");
+        } else {
+            printf("O Área da cidade 1: %.2f\n",areaa1);
+            printf("O Área da cidade 2: %.2f\n",areaa2);
+            printf("Empate!!! \n");
+        }
+        break;
+        case 7:
+        printf("Saindo...\n");
+        break;
+        default:
+        printf("opção inválida. Tente Novamente.\n!!!");
+    }
     return 0;
 }
